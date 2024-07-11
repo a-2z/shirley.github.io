70ec55258f7f569e96691183439775f487ee976d
ivanalan <10alanm@gmail.com>
2024-07-11 00:25:18 -0400

    add json data

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,13 +6,6 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    {
-      resolve: `gatsby-plugin-react-router`,
-      options: {
-        // If you need to pass options to react-router-dom, you can do so here
-      },
-    },
-    // Add other plugins as needed
   ],
   
   
