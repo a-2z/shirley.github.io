d64338a3c4845b5a100c8e9b6b08a732e71ae678
az359 <az359@cornell.edu>
2024-07-13 04:10:36 -0400

    Finish scrapbook and add backtrack

--- src/pages/scrapbook.js
+++ src/pages/scrapbook.js
@@ -1,5 +1,16 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 import "../styles/scrapbook-styles.css";
+
+import useWindowDimensions from "../hooks/ScrapbookHooks"
+
+// Components
+import Desk from "../components/Desk"
+import Polaroid from "../components/Polaroid";
+
+// Data
+import scrapbookData from "../data/scrapbookData";
+import audioFile from "../audio/sound_effect_1.mp3";
 
 //import SCRAPBOOK_COVER from
 /**
@@ -13,90 +24,169 @@ import "../styles/scrapbook-styles.css";
  *  3) Message: an unframed 'image' of a handwritten message
  * 
  * With the exception of the cover, every page will contain a mix of all 3.
- */
+*/
 
-
-class Scrapbook extends React.Component {
-    numPages = 0
-    // constructor(page_number, images, messages) {
-        
-    // }
-    constructor(props) {
-        super(props);
-        this.state = { numPages: 0 };
-        // Bind the handleClick method to the component's context
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    render() {
-        if (this.state.numPages == 0) {
-            return (<ScrapbookCover/>)
-        }
-        else {
-            return (<p>numPages</p>)
-        }
-    }
-
-    handleClick() {
-        this.setState((prevState) => ({
-            numPages: prevState.numPages + 1
-        }));
-    }
+function ScrapbookCover() {
+    return (
+    <div className="scrapbook-cover">
+        <div className="scrapbook-cover-text-container"></div>
+    </div>
+    );
 }
 
-class ScrapbookCover extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { numPages: 0 };
+function ScrapbookContentPage(props) {
+    const {height, width} = useWindowDimensions();
+    /**
+     * Calculate whether width of viewport is sufficient to display
+     * two pages side-by-side, given an aspect ratio of 4:3 and max
+     * utilization of vertical height for pages.
+     */
+    function isDesktop(height, width) {
+        // Divide by aspect ratio
+        var minLandscapePageWidth = height / 4 * 3;
+        const minPadding = 20;
+        return width > (minLandscapePageWidth * 2 + minPadding)
     }
-    render(){
-        return (<div class="scrapbook-cover">'
-            <div class="scrapbook-cover-text-container">
-            <h2>Scrapbook</h2>
-            <h3>Shirley</h3>
-            </div>
-        </div>)
-    }
-}
 
+    var imagesPerPage = isDesktop(height, width) ? 4 : 2;
+    if (props.pageNumber < Math.floor(scrapbookData.length / imagesPerPage) + 
+        (scrapbookData.length % imagesPerPage == 0 ? 0 : 1)) {
 
-class ScrapbookItem {}
-class TapedScrapbookItem extends ScrapbookItem{}
-class WrittenScrapbookItem extends ScrapbookItem{}
-
-// Used for headers and titles -- analogous to <h*> tags
-class ScrapbookHeader extends WrittenScrapbookItem {}
-// Used for small handwritten notes -- analogous to <p> tags
-class ScrapbookNote extends WrittenScrapbookItem {}
-// Image in a polaroid frame
-class ScrapbookPolaroid extends TapedScrapbookItem {}
-// Image with a small white border padding
-class ScrapbookPrint extends TapedScrapbookItem {}
-// Unframed image (handwriting)
-class ScrapbookMessage extends TapedScrapbookItem {}
-
-class Desk extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
+        var pageContent = scrapbookData.slice((props.pageNumber - 1) * imagesPerPage, 
+                                               Math.min(scrapbookData.length, 
+                                                        (props.pageNumber - 1) * imagesPerPage + imagesPerPage));
         return (
-            <div class="scrapbook-desktop">
-                {this.props.children}
+            <div className="scrapbook-content-page">
+                {pageContent.map((item, index) => {
+                    switch (item.type) {
+                        case 'polaroid':
+                            var rotationDirection = Math.random() < 0.5 ? 1 : -1;
+                            return <Polaroid 
+                                        key={index} 
+                                        image={item.image} 
+                                        note={item.note} 
+                                        date={item.date}
+                                        xOffset={Math.random() * 5} 
+                                        yOffset={Math.random() * 5} 
+                                        rotationAngle={Math.random() * 2 * rotationDirection}
+                            />;
+                        case 'message':
+                            alert(item.content)
+                            return <div>
+                                <h3>{item.content}</h3>
+                            </div>
+                        default:
+                            return null;
+                    }
+                })}
             </div>
         );
+    } else {
+        return <></>
     }
+}
+
+function Scrapbook() {
+    const {height, width} = useWindowDimensions();
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const scrapbookContentRef = useRef(null);
+
+    useEffect(() => {
+        scrapbookContentRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        const audio = new Audio(audioFile);
+        audio.play();
+    }, []);
+
+    /**
+     * Calculate whether width of viewport is sufficient to display
+     * two pages side-by-side, given an aspect ratio of 4:3 and max
+     * utilization of vertical height for pages.
+     */
+    function isDesktop(height, width) {
+        // Divide by aspect ratio
+        var minLandscapePageWidth = height / 4 * 3;
+        const minPadding = 20;
+        return width > (minLandscapePageWidth * 2 + minPadding)
+    }
+
+    function flipPageForward() {
+        const step = isDesktop(height, width) ? 2 : 1;
+        // coincidence: step and images per page are not actually logically
+        // related; it's 3 am
+        var imagesPerPage = step * 2;
+        if (pageNumber == 0)
+            setPageNumber(pageNumber + 1)
+        else if (pageNumber < Math.floor(scrapbookData.length / imagesPerPage) + 
+                (scrapbookData.length % imagesPerPage == 0 ? 0 : 1) - step)
+            setPageNumber(pageNumber + step)
+    }
+    
+    function flipPageBackward() {
+        const step = isDesktop(height, width) ? 2 : 1;
+        // coincidence: step and images per page are not actually logically
+        // related; it's 3 am
+        var imagesPerPage = step * 2;
+        if (pageNumber === 1)
+            setPageNumber(0)
+        else if (pageNumber > 0)
+            setPageNumber(pageNumber - step)
+    }
+
+    function handleClick(event) {
+        const rect = event.target.getBoundingClientRect();
+        const elementWidth = rect.width;
+        const clickX = event.clientX - rect.left;
+
+        if (clickX <= elementWidth / 2) {
+            flipPageBackward();
+        } else {
+            flipPageForward();
+        }
+    } 
+
+    function handleKeyDown(event) {
+        if (event.key == "ArrowRight" || event.key == " ") {
+            flipPageForward();
+        } else if (event.key == "ArrowLeft") {
+            flipPageBackward();
+        }
+
+    }
+    
+    if (isDesktop(height, width)) {
+        var pages = (        
+        <div className="scrapbook-content-container" tabIndex={0} onClick={handleClick} onKeyDown={handleKeyDown}>
+            {pageNumber === 0 ? <ScrapbookCover/> : <div className="scrapbook-dual-page">
+                                                        <ScrapbookContentPage pageNumber={pageNumber}/>
+                                                        <ScrapbookContentPage pageNumber={pageNumber+1}/>
+                                                    </div>}
+        </div> )
+    } else {
+        var pages = (
+        <div className="scrapbook-content-container" ref={scrapbookContentRef} tabIndex={0} onClick={handleClick} onKeyDown={handleKeyDown}>
+            {pageNumber === 0 ? <ScrapbookCover/> : <ScrapbookContentPage pageNumber={pageNumber}/>}
+        </div> )
+    }
+    return (
+        <div className="scrapbook-content-container" ref={scrapbookContentRef} tabIndex={0} onClick={handleClick} onKeyDown={handleKeyDown}>
+            {pageNumber === 0 ? <ScrapbookCover/> : pages}
+        </div> 
+    );
 }
 
 const ScrapbookPage = () => {
     return (
-    <main class="scrapbook-viewer">
+    <main className="scrapbook-viewer">
         <Desk>
             <Scrapbook/>
         </Desk>
     </main>);
-}
+};
 
-export default ScrapbookPage
+export default ScrapbookPage;
 
 export const Head = () => <title>Scrapbook</title>  
